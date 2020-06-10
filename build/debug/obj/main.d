f1fe83bbd8427a7c9cfd739b407ac0323929e95e
norrbotten <axelsp@gmail.com>
2020-06-10 19:17:39 +0200

    zoomy type trait polymorphism-ish thing

---
+++ build/debug/obj/main.d
@@ -0,0 +1,13 @@
+debug/obj/main.o: ../src/main.cpp ../src/integrators/albedo.hpp \
+ ../src/accel/accel_trait.hpp ../src/accel/naive.hpp \
+ ../src/accel/geometry.hpp ../src/integrators/integrator_trait.hpp
+
+../src/integrators/albedo.hpp:
+
+../src/accel/accel_trait.hpp:
+
+../src/accel/naive.hpp:
+
+../src/accel/geometry.hpp:
+
+../src/integrators/integrator_trait.hpp:
